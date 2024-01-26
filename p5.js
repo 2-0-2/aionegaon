7f03a8937d49081ba90c5028e1f6c133560ee839
rkdalswl718 <rkdalswl070718@gmail.com>
2024-01-26 11:01:48 +0900

    k

--- p5.js
+++ p5.js
@@ -1,36 +1,56 @@
-let video;
-let poseNet;
-let poses = [];
+const URL = "./model/";
+let model, webcam, ctx, labelContainer, maxPredictions;
 
-function setup() {
-  createCanvas(640, 480);
-  video = createCapture(VIDEO);
-  video.size(width, height);
+async function init() {
+  const modelURL = URL + "model.json";
+  const metadataURL = URL + "metadata.json";
 
-  // Teachable Machine Pose 모델 로드
-  const poseModelURL = "./model";
-  poseNet = ml5.poseNet(video, poseModelURL, modelLoaded);
-}
+  model = await tmPose.load(modelURL, metadataURL);
+  maxPredictions = model.getTotalClasses();
 
-function modelLoaded() {
-  console.log("Teachable Machine Pose Model Loaded!");
-}
+  const size = 100;
+  const flip = true;
+  webcam = new tmPose.Webcam(size, size, flip);
+  await webcam.setup();
+  await webcam.play();
+  window.requestAnimationFrame(loop);
 
-function draw() {
-  image(video, 0, 0, width, height);
-
-  // 캡처된 프레임에서 손동작 감지
-  if (poses.length > 0) {
-    const keypoints = poses[0].pose.keypoints;
-
-    // 여기에서 keypoints를 사용하여 손동작 처리 가능
-    console.log(keypoints);
+  const canvas = document.getElementById("canvas");
+  canvas.width = size;
+  canvas.height = size;
+  ctx = canvas.getContext("2d");
+  labelContainer = document.getElementById("label-container");
+  for (let i = 0; i < maxPredictions; i++) {
+    labelContainer.appendChild(document.createElement("div"));
   }
 }
 
-function gotPoses(results) {
-  poses = results;
+async function loop(timestamp) {
+  webcam.update();
+  await predict();
+  window.requestAnimationFrame(loop);
 }
 
-// Teachable Machine Pose 이벤트 리스너 등록
-poseNet.on("pose", gotPoses);
+async function predict() {
+  const { pose, posenetOutput } = await model.estimatePose(webcam.canvas);
+  const prediction = await model.predict(posenetOutput);
+
+  for (let i = 0; i < maxPredictions; i++) {
+    const classPrediction =
+      prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+    labelContainer.childNodes[i].innerHTML = classPrediction;
+  }
+
+  drawPose(pose);
+}
+
+function drawPose(pose) {
+  if (webcam.canvas) {
+    ctx.drawImage(webcam.canvas, 0, 0);
+    if (pose) {
+      const minPartConfidence = 0.5;
+      tmPose.drawKeypoints(pose.keypoints, minPartConfidence, ctx);
+      tmPose.drawSkeleton(pose.keypoints, minPartConfidence, ctx);
+    }
+  }
+}
